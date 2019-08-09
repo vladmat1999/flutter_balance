92feef361bdad42b6134b1b48d8c17b8e50000fa
Vlad Ianculescu <vladmatei99@yahoo.com>
2019-08-09 15:20:19 +0300

    Initial commit

---
+++ build/app/intermediates/flutter/release/android-arm/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: C:\Users\vladm\AppData\Local\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <shared_preferences/SharedPreferencesPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+}
+
+@end
