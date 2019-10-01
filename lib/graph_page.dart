f4517b92db2bd4123b2c9500804c7a1fcebd935c
Vlad Ianculescu <vlad.ianculescu@student.manchester.ac.uk>
2019-10-01 21:37:24 +0100

    Refactored and commented graph page

--- lib/graph_page.dart
+++ lib/graph_page.dart
@@ -21,6 +21,9 @@ class GraphPageState extends State<GraphPage>
   ///This part of the code is used to control the initial zoom and pan
   ///on the graph. It plots a ingle grph point 
   List<ListTile> entries = <ListTile>[];
+
+  ///Used to handle the minimum and maximum points when the graph has
+  ///less than 10 entries
   var data = [
     GraphPoint(0,0),
   ];
@@ -28,23 +31,22 @@ class GraphPageState extends State<GraphPage>
   var minPoint = 0;
   var maxPoint = 10;
 
-  ScrollController _controller;
-
+  ///Initialize the state by updating pulling 
+  ///data from the database and updating the chart
+  ///and scroll controller
   @override
   void initState() {
     super.initState();
     updateChart();
-    _controller = ScrollController();
   }
 
   @override
   Widget build(BuildContext context) {
-    minPoint = minPoint ?? 0;
-    maxPoint = maxPoint ?? 10;
 
+    //Build a new series of graph points
     var series = [
       new Series(
-        id : "Clicks",
+        id : "Balance",
         domainFn: (GraphPoint gp, _) => gp.x,
         measureFn: (GraphPoint gp, _) => gp.y,
         data: data
@@ -55,114 +57,159 @@ class GraphPageState extends State<GraphPage>
       appBar: AppBar(
         title: Text("Details"),
       ),
+
       backgroundColor: Colors.white,
+
+      ///The body consists of a FutureBuilder used to 
+      ///wait for the data to be pulled from the db
       body: FutureBuilder<List<TableEntry>>(
+
+              ///The builder will be based on a Future
+              ///containing the db entries
               future: dbHelper.getEntries(),
 
               builder: (context, snapshot) {
+                ///Set a loading indicator
                 if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
 
-                //s_controller.addListener(_scrollListener());
-                //_controller.addListener(_scrollListener());
-
+                ///The data comes from the daatabase from old to new, so it has
+                ///to be reversed to get data from new to old
                 var newData = snapshot.data.reversed.toList();
 
+                ///The body consists of two columns containing the table and
+                ///some separators
                 return Center(child: Column(
                   children: <Widget>[
+
+                    //The first object is the line graph
                     Container(
                       height: MediaQuery.of(context).size.height / 3,
                       width: MediaQuery.of(context).size.width * 0.9,
                       child: LineChart(series, 
                       animate: true,
+                      ///Zoom the axis between min and max points
                       domainAxis: NumericAxisSpec(
                         viewport: NumericExtents.fromValues([minPoint, maxPoint]),
                         showAxisLine: true,
                       ),
+                      //Add the pan and zoom behaviour
                       behaviors: [PanAndZoomBehavior()],
                       )
                     ),
 
+                    ///The Expanded widget contains the list of the transsactions
+                    ///and fills the rest of the screen
                     Expanded(
+                      
+                      ///Build the listview
                       child: ListView.builder(
-                            controller: _controller,
-                            itemBuilder: (context, i) {
-                    
-                    if(i ~/ 2 >= newData.length)
-                      return null;
+                        itemBuilder: (context, i) {
 
-                    else if(i.isOdd)
-                      return Divider(color: Colors.grey,);
+                          ///Stop the builder when the list ends
+                          if(i ~/ 2 >= newData.length)
+                            return null;
 
-                    var item = newData[i ~/ 2];
-                    
-                    return ListTile(
-                    title:Row(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Text(DateFormatter.formatDate(item.date), style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15
-                            )),
-                            Padding(padding: EdgeInsets.only(bottom: 2),),
-                            Text(DateFormatter.formatTime(item.date), style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15
-                            )),
-                          ],
-                        ),
+                          ///On odd numbered add a divider
+                          else if(i.isOdd)
+                            return Divider(color: Colors.grey,);
 
-                        Padding(padding: EdgeInsets.symmetric(horizontal: 4),),
+                          ///Since we add data on even numbers, the 
+                          ///data item will be i/2
+                          var item = newData[i ~/ 2];
+                          
+                          ///Generate the list tile
+                          ///It is sepparated into 3 rows, the first and
+                          ///last each containing 2 columns and other separators
+                          return ListTile(
+                            title:Row(
 
-                        Expanded(
-                          child: Center(
-                                        child: Text(item.location, style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15
-                            ), overflow: TextOverflow.ellipsis, maxLines: 1,),
-                          ),
-                        ),
+                              ///This comlumn divides the date into date and time
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ///Format the date
+                                    Text(DateFormatter.formatDate(item.date), 
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15
+                                      )
+                                    ),
 
-                        Padding(padding: EdgeInsets.symmetric(horizontal: 4),),
+                                    Padding(padding: EdgeInsets.only(bottom: 2),),
 
-                        Container(
-                          child: Column(
-                            children: <Widget>[
-                              Text(formatAmount(item.amount), style: TextStyle(
-                                              color: getAmountColor(item.amount),
-                                            )
-                                          ),
+                                    ///Format the time
+                                    Text(DateFormatter.formatTime(item.date), style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15
+                                      )
+                                    ),
+                                  ],
+                                ),
 
-                              Container(
-                                color: Colors.black,
-                                height: 2,
-                                padding: EdgeInsets.symmetric(vertical: 10),
+                              Padding(padding: EdgeInsets.symmetric(horizontal: 4),),
+
+                              ///The expanded widget will fill the center row
+                              ///It holds the location
+                              Expanded(
+                                child: Center(
+                                    child: Text(item.location, 
+                                      style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15
+                                    ),
+                                   overflow: TextOverflow.ellipsis, maxLines: 1,
+                                   ),
+                                ),
                               ),
 
-                              Text(item.ballance.toInt().toString(), style: TextStyle(color: Colors.grey))
-                            ],
-                          ),
+                              Padding(padding: EdgeInsets.symmetric(horizontal: 4),),
 
-                          padding: EdgeInsets.only(right: 10),
-                        ),
+                              ///The third widget contains the balance and amount spend/earned
+                              Container(
+                                child: Column(
+                                  children: <Widget>[
+                                    ///It also formats the amount
+                                    Text(formatAmount(item.amount), 
+                                      style: TextStyle(
+                                        color: getAmountColor(item.amount),
+                                      )
+                                    ),
 
-                        Divider(color: Colors.black,)
-                        
-                      ],
-                    )
-                    );
-                  },
-                ),
-              )
-            ]
-          )
-        );
+                                    Container(
+                                      color: Colors.black,
+                                      height: 2,
+                                      padding: EdgeInsets.symmetric(vertical: 10),
+                                    ),
 
-      },
-    )    
+                                    ///Get the balance and format it
+                                    Text(item.ballance.toInt().toString(), 
+                                      style: TextStyle(
+                                        color: Colors.grey
+                                        )
+                                      )
+                                  ],
+                                ),
+
+                                  padding: EdgeInsets.only(right: 10),
+                              ),
+
+                              Divider(color: Colors.black,)
+                                
+                          ],
+                        )
+                      );
+                    },
+                  ),
+                )
+              ]
+            )
+          );
+        },
+      )    
     );
   }
 
+  ///Formats the amount spend/earned text
   String formatAmount(double amount)
   {
     String text = amount.toStringAsFixed(amount.truncateToDouble() == amount ? 0 : 2);
@@ -170,6 +217,7 @@ class GraphPageState extends State<GraphPage>
     return text; 
   }
 
+  ///Helper function for formatAmount
   getAmountColor(double amount)
   {
     if(amount > 0)
@@ -179,31 +227,28 @@ class GraphPageState extends State<GraphPage>
     else return Colors.grey;
   }
 
+  ///Used to pull data from the database and update the chart
   updateChart() async 
   {
+    //Get all the entries from the table and ensure they are not empty
     List<TableEntry> te = await dbHelper.getEntries();
     if(te == null || te.length == 0 )
       return;
-    
+
     setState(() {
+     //Sets the min and max point to be displayed on screen
      maxPoint = te.length;
      minPoint = maxPoint - 10 > 0 ? maxPoint - 10 : 0;
 
-     print("asd" + maxPoint.toString());
-     print("min" + minPoint.toString());
-
+     //Generate the graph points
      data = List.generate(te.length, (i){
        return GraphPoint(i,te[i].ballance);
      });
     });
   }
-
-  _scrollListener()
-  {
-    print(_controller.offset);
-  }
 }
 
+///Data structure representing a graph point
 class GraphPoint
 {
   final int x;
